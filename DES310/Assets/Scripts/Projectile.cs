607fc8f6335a0f38906fa7fb8b1e178b9804e751
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-01-24 12:17:57 +0000

    Yup comments

--- DES310/Assets/Scripts/Projectile.cs
+++ DES310/Assets/Scripts/Projectile.cs
@@ -22,13 +22,17 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get current position
         Vector2 oldPos = transform.position;
         float distance = speed * Time.deltaTime;
+        // Get new desired position
         Vector2 newPos = oldPos + direction * distance;
         RaycastHit2D[] hits;
+        // Raycast along travel path
         hits = Physics2D.LinecastAll(oldPos, newPos);
         foreach (RaycastHit2D hit in hits )
         {
+            // Don't hit the player that shot the bullet
             if (hit.collider.gameObject != owner.gameObject)
             {
                 Debug.Log("Hit " + hit.collider.gameObject.name);
@@ -36,19 +40,23 @@ public class Projectile : MonoBehaviour
                 break;
             }
         }
+        // Move to new position
         transform.position = newPos;
     }
 
     public void SetDirection(Vector2 newDirection)
     {
+        // Set direction and normalize
         direction = newDirection;
         direction.Normalize();
+        // Reset rotation, then rotate
         transform.rotation = Quaternion.identity;
         transform.Rotate(direction);
     }
 
     public void SetType(ProjectileType type)
     {
+        // Update type and properties
         this.type = type;
         speed = type.speed;
         damage = type.damage;
@@ -57,6 +65,7 @@ public class Projectile : MonoBehaviour
 
     public void SetOwner(GameObject owner)
     {
+        // Track owner to avoid collision with shooter
         this.owner = owner;
     }
 }
