3d4e2e120b803a221f8377ecdba5f314820fcf8f
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-01-31 09:49:05 +0000

    Enemy spawners keep track of spawned enemies and delete them when the spawner is deleted

--- DES310/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
+++ DES310/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -7,13 +7,17 @@ public class SpawnEnemy : MonoBehaviour
     // Enemy Game Objects
     [SerializeField] private Enemy enemyPrefab;
     [SerializeField] private EnemyType enemyType;
+    private List<Enemy> spawnedEnemies;
 
     // Start is called before the first frame update
     void Start()
     {
+        SpawnAnEnemy();
+    }
 
-        Enemy newEnemy = Instantiate(enemyPrefab, new Vector2(transform.position.x, transform.position.y), enemyPrefab.transform.rotation);
-        newEnemy.SetType(enemyType);
+    private void OnDestroy()
+    {
+        DestroySpawnedEnemies();
     }
 
     // Update is called once per frame
@@ -21,9 +25,24 @@ public class SpawnEnemy : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            // Spawn Enemy at spawn point location
-            Enemy newEnemy = Instantiate(enemyPrefab, new Vector2(transform.position.x, transform.position.y), enemyPrefab.transform.rotation);
-            newEnemy.SetType(enemyType);
+            SpawnAnEnemy();
         }
+    }
+
+    void SpawnAnEnemy()
+    {
+        // Spawn Enemy at spawn point location
+        Enemy newEnemy = Instantiate(enemyPrefab, new Vector2(transform.position.x, transform.position.y), enemyPrefab.transform.rotation);
+        newEnemy.SetType(enemyType);
+        spawnedEnemies.Add(newEnemy);
+    }
+
+    void DestroySpawnedEnemies()
+    {
+        foreach (Enemy e in spawnedEnemies)
+        {
+            Destroy(e);
+        }
+        spawnedEnemies.Clear();
     }
 }
