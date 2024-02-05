aabbcf12b4a923cccc7f34a000343c34b421fac0
2200283 <2200283@uad.ac.uk>
2024-02-05 23:46:42 +0000

    State Machine v1.0

--- DES310/Assets/Scripts/Enemy Scripts/State Machine/States/EnemyCHASEState.cs
+++ DES310/Assets/Scripts/Enemy Scripts/State Machine/States/EnemyCHASEState.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class EnemyCHASEState : EnemyState
 {
+    private GameObject Player;
+    private float speed = 2;
+
     public EnemyCHASEState(EnemyBase enemyBase, EnemyStateMachine enemyStateMachine) : base(enemyBase, enemyStateMachine)
     {
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     public override void AnimationTriggerEvent(EnemyBase.AnimationTriggerType triggerType)
@@ -27,15 +31,30 @@ public class EnemyCHASEState : EnemyState
     {
         base.FrameUpdate();
 
-        enemyBase.MoveEnemy(Vector2.zero);
+        if (!enemyBase.IsAggro)
+        {
+            enemyBase.StateMachine.ChangeState(enemyBase.IDLEState);
+        }
     }
 
     public override void PhysicsUpdate()
     {
         base.PhysicsUpdate();
+
+        Chase(Player);
     }
 
     #region State Specific Functions
+
+    private void Chase(GameObject target)
+    {
+        // Follow Target
+        Vector2 direction = target.transform.position - enemyBase.transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        enemyBase.MoveEnemy(direction * speed);
+        enemyBase.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
 
     #endregion
 }
