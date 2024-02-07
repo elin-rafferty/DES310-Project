8b909f62e7092908444fc494d078602bd57790bf
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-02-07 13:37:05 +0000

    Fixed enemy collision

--- DES310/Assets/Sprites/Tile Assets/meow.cs
+++ DES310/Assets/Sprites/Tile Assets/meow.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum playerstates
+{
+    silly,idle,devious,genocidal,george
+}
 public class meow : MonoBehaviour
 {
+    playerstates state = playerstates.silly;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +17,16 @@ public class meow : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        switch(state)
+        {
+            case playerstates.silly:
+                blehhhh();
+                break;
+        }
+    }
+
+    private void blehhhh()
     {
         
     }
