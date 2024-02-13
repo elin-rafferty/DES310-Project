c6dafe5af3cd2e1401042fc3185505fa3830fbf4
2200283 <2200283@uad.ac.uk>
2024-02-13 16:58:09 +0000

    Sqr Magnitude aggro range

--- DES310/Assets/Scripts/Enemy Scripts/Trigger Checks/EnemyAggroCheck.cs
+++ DES310/Assets/Scripts/Enemy Scripts/Trigger Checks/EnemyAggroCheck.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class EnemyAggroCheck : MonoBehaviour
 {
     public GameObject Player { get; set; }
-    private EnemyBase enemy;
+    private EnemyBase enemyBase;
 
     private void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Trigger Check");
-        enemy = GetComponentInParent<EnemyBase>();
+        enemyBase = GetComponentInParent<EnemyBase>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {;
-        if (collision.gameObject == Player)
-        {
-            enemy.SetAggroStatus(true);
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
+    void FixedUpdate()
     {
-        if (collision.gameObject == Player) 
+        float distance = new Vector2(Player.transform.position.x - enemyBase.transform.position.x, Player.transform.position.y - enemyBase.transform.position.y).sqrMagnitude;
+
+        if (distance < 1225)
         {
-            enemy.SetAggroStatus(false);
+            enemyBase.SetAggroStatus(true);
+        }
+        else
+        {
+            enemyBase.SetAggroStatus(false);
         }
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{;
+    //    if (collision.gameObject == Player)
+    //    {
+    //        enemyBase.SetAggroStatus(true);
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject == Player) 
+    //    {
+    //        enemyBase.SetAggroStatus(false);
+    //    }
+    //}
 }
