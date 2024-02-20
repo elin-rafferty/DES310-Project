3b38d04ecb56a988c5e723670974c60f10adfd5d
2200283 <2200283@uad.ac.uk>
2024-02-20 17:31:22 +0000

    New Sprites and epic music track

--- DES310/Assets/Scripts/Enemy Scripts/Behaviour Logic/Chase/EnemySprinterChase.cs
+++ DES310/Assets/Scripts/Enemy Scripts/Behaviour Logic/Chase/EnemySprinterChase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Pathfinding;
 
 [CreateAssetMenu(fileName = "Chase - Sprinter Chase", menuName = "Enemy Logic/Chase Logic/Sprinter Chase")]
