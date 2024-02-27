34ee64519051223ac527c0562eef1d46f2033e03
2200283 <2200283@uad.ac.uk>
2024-02-27 13:14:39 +0000

    Modifier Screen

---
+++ DES310/Assets/Scripts/Enums/ModifierEnum.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Modifiers
+{
+    NO_MODIFIER,
+    ENEMY_HEALTH,
+    ENEMY_DAMAGE,
+    WALKER_SPEED,
+    SPITTER_FIRE_RATE,
+    SPRINTER_SPEED,
+    RESOURCE_DROPRATE,
+}
