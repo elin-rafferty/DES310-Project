97edd99f5c49be8f61383c27525bbc1040b78fd5
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-02-27 21:42:49 +0000

    Added vent timer

---
+++ DES310/Assets/Scripts/UI/VentTimer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VentTimer : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private VentScript vent;
+    private bool textEnabled = false;
+    public float colourChangeTime = 0.5f;
+    private float colourChangeTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vent = GetComponent<VentScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vent.timeBeforeOpen == 0 && !textEnabled)
+        {
+            text.text = "Vent Open!!!!!";
+            text.color = Color.red;
+            colourChangeTimer = colourChangeTime;
+            textEnabled = true;
+        } else if (textEnabled)
+        {
+            colourChangeTimer -= Time.deltaTime;
+            if (colourChangeTimer <= 0)
+            {
+                colourChangeTimer = colourChangeTime;
+                text.color = text.color == Color.red ? Color.yellow : Color.red;
+            }
+        }
+    }
+}
