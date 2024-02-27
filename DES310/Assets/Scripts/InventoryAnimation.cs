2666bbed694430ac1118e0f69b1b44c96e0fc978
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-02-27 12:17:32 +0000

    Cursor is fixed in inventory

--- DES310/Assets/Scripts/InventoryAnimation.cs
+++ DES310/Assets/Scripts/InventoryAnimation.cs
@@ -7,6 +7,7 @@ public class InventoryAnimation : MonoBehaviour
 
     Animator anim;
     bool InventoryOpen = false;
+    public EventHandler eventHandler;
 
 
     // Start is called before the first frame update
@@ -24,6 +25,7 @@ public class InventoryAnimation : MonoBehaviour
             {
                 anim.Play("Open");
                 InventoryOpen = true;
+                eventHandler.InventoryChangeState.Invoke(true);
             }
         }
 
@@ -33,6 +35,7 @@ public class InventoryAnimation : MonoBehaviour
             {
                 anim.Play("Close");
                 InventoryOpen = false;
+                eventHandler.InventoryChangeState.Invoke(false);
             }
         }
 
