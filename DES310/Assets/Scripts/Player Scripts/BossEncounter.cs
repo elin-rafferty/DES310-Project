ac15d5a4eb3d46f1ac9ce6fb60c961898bdb472f
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-03-05 13:14:08 +0000

    Put script in trigger instead of player

--- DES310/Assets/Scripts/Player Scripts/BossEncounter.cs
+++ DES310/Assets/Scripts/Player Scripts/BossEncounter.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BossEncounter : MonoBehaviour
 {
-    [SerializeField] GameObject boss; 
+    [SerializeField] GameObject boss;
+    [SerializeField] HorizontalDoor door;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Boss Trigger"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Trigger");
             // Close Door
@@ -16,7 +17,8 @@ public class BossEncounter : MonoBehaviour
 
             // Set Boss Active
             boss.SetActive(true);
-
+            door.Lock();
+            door.Close();
 
             // Camera Sequence
         }
