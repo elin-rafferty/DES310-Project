10947dcc58f5cc9b75a7d2de3be473bbb827955f
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-03-06 13:13:50 +0000

    Switched to new input system and added inventory controls for controller

---
+++ DES310/Assets/Scripts/UI/VirtualMouseUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.UI;
+
+public class VirtualMouseUI : MonoBehaviour
+{
+    VirtualMouseInput virtualMouseInput;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        virtualMouseInput = GetComponent<VirtualMouseInput>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector2 virtualMousePos = virtualMouseInput.virtualMouse.position.value;
+        virtualMousePos.x = Mathf.Clamp(virtualMousePos.x, 0f, Screen.width);
+        virtualMousePos.y = Mathf.Clamp(virtualMousePos.y, 0f, Screen.height);
+        InputState.Change(virtualMouseInput.virtualMouse.position, virtualMousePos);
+    }
+}
