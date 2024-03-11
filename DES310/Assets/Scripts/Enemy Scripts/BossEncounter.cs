43e333d4bff7fb7e6931c2b9d6c105eee0fc0f38
2200283 <2200283@uad.ac.uk>
2024-03-11 16:05:05 +0000

    boss scream

--- DES310/Assets/Scripts/Enemy Scripts/BossEncounter.cs
+++ DES310/Assets/Scripts/Enemy Scripts/BossEncounter.cs
@@ -76,6 +76,7 @@ public class BossEncounter : MonoBehaviour
             if (boss)
             {
                 boss.SetActive(true);
+                SoundManager.instance.PlaySound(SoundManager.SFX.EnemyScreech, boss.transform, 1f);
 
                 // Close Door
                 door.Lock();
