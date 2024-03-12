29373b77034561a41f2b03fcb7ce0dbafba72c4d
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-03-12 21:14:10 +0000

    Added controls prompt to the inventory

---
+++ DES310/Assets/Scripts/UI/DisplayInventoryControls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInventoryControls : MonoBehaviour
+{
+    [SerializeField] SettingsSO settings;
+    [SerializeField] EventHandler eventHandler;
+    [SerializeField] Text text;
+    bool isInventoryOpen = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        eventHandler.InventoryChangeState.AddListener(SetInventoryState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInventoryOpen)
+        {
+            text.text = (settings.Controls == 0 ? "LMB" : "A") + ": Select Item\n" + (settings.Controls == 0 ? "RMB" : "X") + ": Use / Equip Item";
+        } else
+        {
+            text.text = "";
+        }
+    }
+
+    void SetInventoryState(bool open)
+    {
+        isInventoryOpen = open;
+    }
+}
