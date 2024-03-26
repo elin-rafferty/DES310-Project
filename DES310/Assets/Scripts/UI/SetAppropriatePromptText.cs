0f2a516417ef997bfdbb489e338a3216da991dbe
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2024-03-26 11:38:12 +0000

    Changed interact button from X to A on controller

--- DES310/Assets/Scripts/UI/SetAppropriatePromptText.cs
+++ DES310/Assets/Scripts/UI/SetAppropriatePromptText.cs
@@ -14,7 +14,7 @@ public class SetAppropriatePromptText : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name != ("Main Hub"))
         {
-            text.text = settings.Controls == 0 ? "Press E To Continue" : "Press X To Continue";
+            text.text = settings.Controls == 0 ? "Press E To Continue" : "Press A To Continue";
         }
     }
 }
