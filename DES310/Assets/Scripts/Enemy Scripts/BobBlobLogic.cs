b5e8a78405b9afcc2da87cca858147ecbddc1f25
2200283 <2200283@uad.ac.uk>
2024-03-27 15:48:05 +0000

    Bob blobs fade

--- DES310/Assets/Scripts/Enemy Scripts/BobBlobLogic.cs
+++ DES310/Assets/Scripts/Enemy Scripts/BobBlobLogic.cs
@@ -6,10 +6,24 @@ public class BobBlobLogic : MonoBehaviour
 {
     [SerializeField] private float lifeSpan;
     [SerializeField] private float damagePerSecond;
+    private float timer;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        timer = lifeSpan;
         Destroy(gameObject, lifeSpan);
+    }
+
+    void Update()
+    {
+        if (timer <= 1)
+        {
+            spriteRenderer.color = Color.Lerp(Color.clear, Color.white, timer);
+        }
+
+        timer -= Time.deltaTime;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
