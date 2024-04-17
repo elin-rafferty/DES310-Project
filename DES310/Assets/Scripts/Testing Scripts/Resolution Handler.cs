9db6c75326efb86696f4f36673237635427d5eb9
2200283 <2200283@uad.ac.uk>
2024-04-17 22:54:45 +0100

    Res lock

---
+++ DES310/Assets/Scripts/Testing Scripts/Resolution Handler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionHandler : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
